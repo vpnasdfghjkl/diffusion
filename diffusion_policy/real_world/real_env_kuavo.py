dddfade36f77581e1b6b5fc8df9d3a4129ef33de
root <shiyapeng66@163.com>
2024-11-10 16:19:50 +0800

    modify kuavo env

--- diffusion_policy/real_world/real_env_kuavo.py
+++ diffusion_policy/real_world/real_env_kuavo.py
@@ -30,9 +30,10 @@ import cv2
 from collections import deque
 from scipy.spatial.transform import Rotation as R
 from typing import List, Optional, Union, Dict, Callable
+from tqdm import tqdm  
 
 
-from dynamic_biped.msg import robotArmInfo, recordArmHandPose, robotHandPosition
+from dynamic_biped.msg import robotArmInfo, recordArmHandPose, robotHandPosition, robot_hand_eff
 from dynamic_biped.srv import controlEndHand, controlEndHandRequest
 
 
@@ -40,12 +41,38 @@ from dynamic_biped.srv import controlEndHand, controlEndHandRequest
 
 DEFAULT_OBS_KEY_MAP = {
     "img":{
-        "img01": "/head_camera/color/image_raw/compressed",
+        "img01": {
+            "topic":"/head_camera/color/image_raw/compressed",
+            "msg_type":CompressedImage,
+            }
     },
     "low_dim":{
-        "obs_state_eef_pose": "/drake_ik/real_arm_hand_pose",
-        "obs_state_hand": "/robot_hand_position",
-        "obs_cmd_eef_pose": "/drake_ik/cmd_arm_hand_pose",
+        "cmd_eef": {
+            "topic":"/drake_ik/cmd_arm_hand_pose", 
+            "msg_type":recordArmHandPose,
+            },
+        "state_eef": {
+            "topic":"/drake_ik/real_arm_hand_pose",
+            "msg_type":recordArmHandPose,
+            },
+        
+        "cmd_joint": {
+            "topic":"/kuavo_arm_traj",
+            "msg_type":JointState 
+            },
+        "state_joint": {
+            "topic":"/robot_arm_q_v_tau",
+            "msg_type":robotArmInfo,
+            },
+        
+        "cmd_gripper": {
+            "topic":"/robot_hand_eff",
+            "msg_type":robot_hand_eff,
+            },
+        "state_gripper": {
+            "topic":"/robot_arm_q_v_tau",
+            "msg_type":robotHandPosition,
+            },
     },
 }
 
@@ -61,9 +88,12 @@ HAND_CLOSE_STATE = "[30, 30, 90, 90, 90, 90, 0, 0, 0, 0, 0, 0]"
 
     
 class ObsBuffer:
-    def __init__(self, img_buffer_size: int = 30, robot_state_buffer_size: int = 120):
+    def __init__(self, img_buffer_size: int = 30, robot_state_buffer_size: int = 100, obs_key_map: Optional[Dict[str, Dict[str, str]]] = None) -> None:
         self.img_buffer_size = img_buffer_size
         self.robot_state_buffer_size = robot_state_buffer_size
+        self.obs_key_map = obs_key_map if obs_key_map is not None else DEFAULT_OBS_KEY_MAP
+        
+        
         self.obs_buffer_data = {key: {"data": deque(maxlen=img_buffer_size),"timestamp": deque(maxlen=img_buffer_size),} \
                                 for key in self.obs_key_map["img"]}
         
@@ -71,76 +101,168 @@ class ObsBuffer:
                                     for key in self.obs_key_map["low_dim"]})
      
         # Subscribe to the ROS topics
-        self.img01_suber = rospy.Subscriber(self.obs_key_map["img"]["img01"],Image,lambda msg: self.image_callback(msg, "img01"),)
         
-        self.obs_state_hand_suber = rospy.Subscriber(DEFAULT_OBS_KEY_MAP["low_dim"]["obs_state_hand"],robotHandPosition,lambda msg: self.left_control_hand(msg, "obs_state_hand"),)
+        
+        
+        def setup_subscribers(obs_key_map, callback_key_map):
+            for obs_cls, topics in obs_key_map.items():
+                for topic_key, topic_info in topics.items():
+                    topic_name = topic_info["topic"]
+                    msg_type = topic_info["msg_type"]
 
-        self.obs_state_eef_pose_suber = rospy.Subscriber(DEFAULT_OBS_KEY_MAP["low_dim"]["obs_state_eef_pose"],recordArmHandPose,lambda msg: self.eef_pose_callback(msg, "obs_state_eef_pose"),)
-        self.obs_cmd_eef_pose_suber = rospy.Subscriber(DEFAULT_OBS_KEY_MAP["low_dim"]["obs_cmd_eef_pose"],recordArmHandPose,lambda msg: self.eef_pose_callback(msg, "obs_cmd_eef_pose"),)
+                    callback = callback_key_map.get(msg_type)
+                    if callback:
+                        self.suber_dict[topic_key] = rospy.Subscriber(topic_name, msg_type, lambda msg, key=topic_key: callback(msg, key))
+                        print(f"Subscribed to {topic_name} with callback {callback.__name__}")
+                    else:
+                        print(f"No callback found for message type {msg_type}")
+        
+        def compressedImage_callback(self, msg: CompressedImage, key: str):
+            np_arr = np.frombuffer(msg.data, np.uint8)
+            cv_img = cv2.imdecode(np_arr, cv2.IMREAD_COLOR)
+            resized_img = cv2.resize(cv_img, (256, 256))
 
-    def image_callback(self, msg: CompressedImage, key: str):
-        np_arr = np.frombuffer(msg.data, np.uint8)
-        cv_img = cv2.imdecode(np_arr, cv2.IMREAD_COLOR)
-        resized_img = cv2.resize(cv_img, (256, 256))
+            self.obs_buffer_data[key]["data"].append(resized_img)
+            self.obs_buffer_data[key]["timestamp"].append(msg.header.stamp.to_sec())
 
-        self.obs_buffer_data[key]["data"].append(resized_img)
-        self.obs_buffer_data[key]["timestamp"].append(msg.header.stamp.to_sec())
+        # def left_control_hand(self, msg: robotHandPosition, key: str):
+        #     left_hand_pose = msg.left_hand_position
+        #     if left_hand_pose[-1] == 0:
+        #         grip = 0
+        #     elif left_hand_pose[-1] == 90:
+        #         grip = 1
+        #     else:
+        #         print("hand pose error")
+        #     self.obs_buffer_data[key]["data"].append(grip)
+        #     self.obs_buffer_data[key]["timestamp"].append(msg.header.stamp.to_sec())
 
-    def left_control_hand(self, msg: robotHandPosition, key: str):
-        left_hand_pose = msg.left_hand_position
-        if left_hand_pose[-1] == 0:
-            grip = 0
-        elif left_hand_pose[-1] == 90:
-            grip = 1
-        else:
-            print("hand pose error")
-        self.obs_buffer_data[key]["data"].append(grip)
-        self.obs_buffer_data[key]["timestamp"].append(msg.header.stamp.to_sec())
+        def recordArmHandPose_callback(self, msg: recordArmHandPose, key: str):
+            # Float64Array ()
+            xyz = np.array(msg.left_pose.pos_xyz)
+            xyzw = np.array(msg.left_pose.quat_xyzw)
+            rotation = R.from_quat(xyzw)
+            euler_angles = rotation.as_euler("xyz")
+            xyzrpy = np.concatenate((xyz, euler_angles))
 
-    def eef_pose_callback(self, msg: recordArmHandPose, key: str):
-        xyz = np.array(msg.left_pose.pos_xyz)
-        xyzw = np.array(msg.left_pose.quat_xyzw)
-        rotation = R.from_quat(xyzw)
-        euler_angles = rotation.as_euler("xyz")
-        xyzrpy = np.concatenate((xyz, euler_angles))
-
-        self.obs_buffer_data[key]["data"].append(xyzrpy)
-        self.obs_buffer_data[key]["timestamp"].append(msg.header.stamp.to_sec())
-
+            self.obs_buffer_data[key]["data"].append(xyzrpy)
+            self.obs_buffer_data[key]["timestamp"].append(msg.header.stamp.to_sec())
+            
+        def joint_callback(self, msg: JointState, key: str):
+            # Float64Array ()
+            joint = (msg.position)[:7]
+            self.obs_buffer_data[key]["data"].append(joint)
+            self.obs_buffer_data[key]["timestamp"].append(msg.header.stamp.to_sec())
+        
+        def robotArmInfo_callback(self, msg: robotArmInfo, key: str):
+            # Float64Array ()
+            joint = msg.q
+            self.obs_buffer_data[key]["data"].append(joint)
+            self.obs_buffer_data[key]["timestamp"].append(msg.header.stamp.to_sec())
+        
+        def robot_hand_eff_callback(self, msg: robot_hand_eff, key: str):
+            # Float32Array (12)
+            joint = msg.data
+            self.obs_buffer_data[key]["data"].append(joint)
+            self.obs_buffer_data[key]["timestamp"].append(msg.header.stamp.to_sec())
+        
+        def robotHandPosition_callback(self, msg: robotHandPosition, key: str):
+            # Uint8Array (6) + Uint8Array (6)
+            joint = msg.left_hand_position + msg.right_hand_position
+            self.obs_buffer_data[key]["data"].append(joint)
+            self.obs_buffer_data[key]["timestamp"].append(msg.header.stamp.to_sec())
+            
+        self.suber_dict = {}
+        
+        DEFAULT_CALLBACK_KEY_MAP = {
+            CompressedImage: compressedImage_callback,
+            recordArmHandPose: recordArmHandPose_callback,
+            JointState: joint_callback,
+            robotArmInfo: robotArmInfo_callback,
+            robot_hand_eff: robot_hand_eff_callback,
+            robotHandPosition: robotHandPosition_callback
+        }
+        self.callback_key_map = DEFAULT_CALLBACK_KEY_MAP
+        setup_subscribers(self.obs_key_map, self.callback_key_map)
+    
+    
+    
+    
+    
     def obs_buffer_is_ready(self):
-        print(len(self.obs_buffer_data["obs_img01"]["data"]))
-        print(len(self.obs_buffer_data["obs_state_hand"]["data"]))
-        print(len(self.obs_buffer_data["obs_state_eef_pose"]["data"]))
-        print(len(self.obs_buffer_data["obs_cmd_eef_pose"]["timestamp"]))
-        return all([len(self.obs_buffer_data[key]["data"]) == self.img_buffer_size for key in DEFAULT_OBS_KEY_MAP if "obs_img" in key]) and \
-               all([len(self.obs_buffer_data[key]["data"]) == self.robot_state_buffer_size for key in DEFAULT_OBS_KEY_MAP if "obs_img" not in key])
+        # print(len(self.obs_buffer_data["obs_img01"]["data"]))
+        # print(len(self.obs_buffer_data["state_gripper"]["data"]))
+        # print(len(self.obs_buffer_data["state_eef"]["data"]))
+        # print(len(self.obs_buffer_data["cmd_eef"]["timestamp"]))
+        return all([len(self.obs_buffer_data[key]["data"]) == self.img_buffer_size for key in DEFAULT_OBS_KEY_MAP["img"]]) and \
+               all([len(self.obs_buffer_data[key]["data"]) == self.robot_state_buffer_size for key in DEFAULT_OBS_KEY_MAP["low_dim"]])
 
     def stop_subscribers(self):
-        self.obs_img01_suber.unregister()
-        self.obs_state_hand_suber.unregister()
-        self.obs_state_eef_pose_suber.unregister()
-        self.obs_cmd_eef_pose_suber.unregister()
+        for key, suber in self.suber_dict.items():
+            suber.unregister()
 
     def get_lastest_k_img(self, k: int) -> Dict[int, Dict[str, np.ndarray]]:
         out = {}
-        for i, key in enumerate(DEFAULT_OBS_KEY_MAP):
-            if "obs_img" in key:
-                out[i] = {
-                    "color": np.array(list(self.obs_buffer_data[key]["data"])[-k:]),
-                    "timestamp": np.array(list(self.obs_buffer_data[key]["timestamp"])[-k:]),
-                }
+        for i, key in enumerate(self.obs_key_map["img"]):
+            out[i] = {
+                "color": np.array(list(self.obs_buffer_data[key]["data"])[-k:]),
+                "timestamp": np.array(list(self.obs_buffer_data[key]["timestamp"])[-k:]),
+            }
         return out
 
     def get_latest_k_robotstate(self, k: int) -> dict:
         out = {}
-        for i, key in enumerate(DEFAULT_OBS_KEY_MAP):
-            if "obs_img" not in key:
-                out[key] = {
-                    "data": np.array(list(self.obs_buffer_data[key]["data"])[-k:]),
-                    "robot_receive_timestamp": np.array(list(self.obs_buffer_data[key]["timestamp"])[-k:]),
-                }
+        for i, key in enumerate(self.obs_key_map["low_dim"]):
+            out[key] = {
+                "data": np.array(list(self.obs_buffer_data[key]["data"])[-k:]),
+                "robot_receive_timestamp": np.array(list(self.obs_buffer_data[key]["timestamp"])[-k:]),
+            }
         return out
+    
+    def wait_buffer_ready(self):
+        progress_bars = {}
+        position = 0
+        for key in self.obs_key_map["img"]:
+            progress_bars[key] = tqdm(total=self.img_buffer_size, desc=f"Filling {key}", position=position, leave=True)
+            position += 1
 
+        for key in self.obs_key_map["low_dim"]:
+            progress_bars[key] = tqdm(total=self.robot_state_buffer_size, desc=f"Filling {key}", position=position, leave=True)
+            position += 1
+
+
+        while not self.obs_buffer_is_ready():
+            for key in self.obs_key_map["img"]:
+                current_len = len(self.obs_buffer_data[key]["data"])
+                progress_bars[key].n = current_len
+                progress_bars[key].refresh()
+
+            for key in self.obs_key_map["low_dim"]:
+                current_len = len(self.obs_buffer_data[key]["data"])
+                progress_bars[key].n = current_len
+                progress_bars[key].refresh()
+
+            time.sleep(0.1)  
+            
+        # 强制将所有进度条填满
+        for key in self.obs_key_map["img"]:
+            progress_bars[key].n = self.img_buffer_size
+            progress_bars[key].refresh()
+
+        for key in self.obs_key_map["low_dim"]:
+            progress_bars[key].n = self.robot_state_buffer_size
+            progress_bars[key].refresh()
+  
+        for bar in progress_bars.values():
+            bar.close()
+      
+        for key in self.obs_key_map["img"]:
+            print(f"{key} buffer size = {len(self.obs_buffer_data[key]['data'])}")
+        for key in self.obs_key_map["low_dim"]:
+            print(f"{key} buffer size = {len(self.obs_buffer_data[key]['data'])}")
+            
+        print("All buffers are ready!")
+        time.sleep(0.5)
+        
 class TargetPublisher:
     def __init__(self):
         self.target_pub = rospy.Publisher(
@@ -198,6 +320,7 @@ class KuavoEnv:
         self.video_capture_resolution = video_capture_resolution
         self.obs_key_map = obs_key_map if obs_key_map is not None else DEFAULT_OBS_KEY_MAP
 
+        self.obs_buffer = ObsBuffer(img_buffer_size=self.img_buffer_size, robot_state_buffer_size=self.robot_state_buffer_size, obs_key_map=self.obs_key_map)
         self.target_publisher = TargetPublisher()
 
 
@@ -260,7 +383,7 @@ class KuavoEnv:
 
         # get data
         # 30 Hz, camera_receive_timestamp
-        k = math.ceil(self.n_obs_steps * (self.video_capture_fps / self.frequency))
+        k_image = math.ceil(self.n_obs_steps * (self.video_capture_fps / self.frequency))
 
         """
         Return order T,H,W,C
@@ -272,7 +395,10 @@ class KuavoEnv:
             1: ...
         }
         """
-        self.last_realsense_data = self.obs_buffer.get_lastest_k_img(k)
+        self.last_realsense_data = self.obs_buffer.get_lastest_k_img(k_image)
+        
+        
+        
         """
         Return order T,D
         {
@@ -283,66 +409,61 @@ class KuavoEnv:
             1: ...
         }
         """
-        last_robot_data = self.obs_buffer.get_latest_k_robotstate(
-            k * (self.robot_publish_rate // self.video_capture_fps)
-        )
+        k_robot = math.ceil(self.n_obs_steps * (self.robot_publish_rate / self.frequency))
+        last_robot_data = self.obs_buffer.get_latest_k_robotstate(k_robot)
         # both have more than n_obs_steps data
 
         # align camera obs timestamps
         dt = 1 / self.frequency
         last_timestamp = np.max(
-            [x["timestamp"][-1] for x in self.last_realsense_data.values()]
+            [x["timestamp"][-2] for x in self.last_realsense_data.values()]
         )
         obs_align_timestamps = last_timestamp - (np.arange(self.n_obs_steps)[::-1] * dt)
 
         camera_obs = dict()
+        camera_obs_timestamps = dict()
         for camera_idx, value in self.last_realsense_data.items():
             this_timestamps = value["timestamp"]
             this_idxs = list()
             for t in obs_align_timestamps:
-                is_before_idxs = np.nonzero(this_timestamps < t)[0]
-                this_idx = 0
-                if len(is_before_idxs) > 0:
-                    this_idx = is_before_idxs[-1]
+                this_idx = np.argmin(np.abs(this_timestamps - t))
+                # is_before_idxs = np.nonzero(this_timestamps < t)[0]
+                # this_idx = 0
+                # if len(is_before_idxs) > 0:
+                #     this_idx = is_before_idxs[-1]
                 this_idxs.append(this_idx)
             # remap key
-            camera_obs[f"camera_{camera_idx}"] = value["color"][this_idxs]
+            camera_obs[f"img0{camera_idx+1}"] = value["color"][this_idxs]
+            camera_obs_timestamps[f"img0{camera_idx+1}"] = this_timestamps[this_idxs]
 
         # align robot obs timestamps
         robot_obs = dict()
         robot_obs_timestamps = dict()
         for robot_state_name, robot_state_data in last_robot_data.items():
-            if robot_state_name in self.obs_topic_key_map:
+            if robot_state_name in self.obs_key_map["low_dim"]:
                 this_timestamps = robot_state_data['robot_receive_timestamp']
                 this_idxs = list()
                 for t in obs_align_timestamps:
-                    is_before_idxs = np.nonzero(this_timestamps < t)[0]
-                    this_idx = 0
-                    if len(is_before_idxs) > 0:
-                        this_idx = is_before_idxs[-1]
+                    this_idx = np.argmin(np.abs(this_timestamps - t))
+                    # is_before_idxs = np.nonzero(this_timestamps < t)[0]
+                    # this_idx = 0
+                    # if len(is_before_idxs) > 0:
+                    #     this_idx = is_before_idxs[-1]
                     this_idxs.append(this_idx)
-                robot_obs[f"robot_state_{robot_state_name}"] = robot_state_data['data'][this_idxs]
-                robot_obs_timestamps[f"robot_state_{robot_state_name}"] = this_timestamps[this_idxs]
+                robot_obs[f"ROBOT_{robot_state_name}"] = robot_state_data['data'][this_idxs]
+                robot_obs_timestamps[f"ROBOT_{robot_state_name}"] = this_timestamps[this_idxs]
 
-        # TODO: after 2024/09/23
-        # accumulate obs
-        if self.obs_accumulator is not None:
-            self.obs_accumulator.put(robot_obs, robot_obs_timestamps)
 
         # return obs
         obs_data = dict(camera_obs)
         
-        
-        history_hand = robot_obs["robot_state_obs_state_hand"]
-        history_eef_pose = robot_obs["robot_state_obs_state_eef_pose"]
-        # add the 1dim hand to 6dim eef_pose
-        
         robot_final_obs = dict()
-        robot_final_obs["state"] = np.concatenate((history_eef_pose, history_hand), axis=1)
+        robot_final_obs["state"] = np.concatenate((robot_obs["ROBOT_state_joint"], robot_obs["ROBOT_state_gripper"]), axis=1)
+   
         obs_data.update(robot_final_obs)
         obs_data["timestamp"] = obs_align_timestamps
         
-        return obs_data
+        return obs_data, camera_obs, camera_obs_timestamps, robot_obs, robot_obs_timestamps
     
     def exec_fake_actions(self, actions: np.ndarray):
         # just print the new actions
@@ -377,3 +498,31 @@ class KuavoEnv:
         # if self.stage_accumulator is not None:
         #     self.stage_accumulator.put(new_stages, new_timestamps)
 
+
+                    
+if __name__ == "__main__":
+    rospy.init_node("test")
+    env = KuavoEnv(img_buffer_size=30, robot_state_buffer_size=100)
+    print("waiting for the obs buffer to be ready ......")
+    env.obs_buffer.wait_buffer_ready()
+    # env.check_timestamps_diff(check_steps=50)
+    # env.save_img_video(check_steps=20)
+    running = True
+
+    while True:
+        # command = input("Enter command (s: start, p: pause, q: exit): ")
+        # if command == 's':
+        #     running = True
+        #     print("Started!")
+        # elif command == 'p':
+        #     running = False
+        #     print("Paused!")
+        # elif command == 'q':
+        #     print("Exiting...")
+        #     break
+
+        if running:
+            cur_obs, _, _, _, _ = env.get_obs()
+            action = cur_obs["agent_pos"]
+            env.exec_actions(actions=action)
+    env.close()
