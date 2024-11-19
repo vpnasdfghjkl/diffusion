080c1310aecf3ab599655461855a622422db7c8c
vpnasdfghjkl <shiyapeng66@163.com>
2024-11-19 23:18:34 +0800

    all info for rosbag

--- read_rosbag_to_zarr.py
+++ read_rosbag_to_zarr.py
@@ -37,8 +37,11 @@ def check_folder(CHECK_PIC_SAVE_FOLDER):
         shutil.rmtree(CHECK_PIC_SAVE_FOLDER)
         os.makedirs(CHECK_PIC_SAVE_FOLDER)
 
-def adjust_pose_rpy(pose):
+def adjust_pose_rpy(lr_pose):
     threshold= math.pi
+    left_pose = []
+    lr_pose = np.array(lr_pose)
+    pose = lr_pose[:,:6]
     pre_eef = pose[0][3:6]  
     for i in range(len(pose)):
         for j in range(3): 
@@ -48,7 +51,24 @@ def adjust_pose_rpy(pose):
             elif diff < -threshold:
                 pose[i][3+j] += 2 * math.pi
             pre_eef[j] = pose[i][3+j]
-    return pose
+    left_pose.append(pose)  #(1, 1976, 6)
+
+    right_pose = []
+    pose = lr_pose[:,6:]
+    pre_eef = pose[0][3:6]  
+    for i in range(len(pose)):
+        for j in range(3): 
+            diff = pose[i][3+j] - pre_eef[j]
+            if diff > threshold:
+                pose[i][3+j] -= 2 * math.pi
+            elif diff < -threshold:
+                pose[i][3+j] += 2 * math.pi
+            pre_eef[j] = pose[i][3+j]
+    right_pose.append(pose) #(1, 1976, 6)
+
+    # (1976,12)
+    all_pose =np.concatenate((left_pose[0],right_pose[0]),axis=1)
+    return np.array(all_pose)
 
 def plot_euler_error(cmd_rot_matrix,state_rot_matrix,base_name):
     euler_error = []
@@ -110,7 +130,9 @@ def use_rosbag_to_show(bag_name):
 
     img=[]
     img_stamp=[]
-    
+    img02=[]
+    img02_stamp=[]
+
     cmd_rot_matrix = []
     state_rot_matrix = []
     delta_rot_matrix =[]
@@ -121,86 +143,78 @@ def use_rosbag_to_show(bag_name):
                                                     '/drake_ik/real_arm_hand_pose', \
                                                     '/robot_hand_eff',\
                                                     '/robot_hand_position',\
-                                                    '/head_camera/color/image_raw/compressed',\
+                                                    '/camera1/color/image_raw/compressed',\
+                                                    '/camera2/color/image_raw/compressed',
                                                   ]):
         # msg_time = msg.header.stamp.to_sec()  # 将时间戳转换为秒
         # if msg_time > end_time:
         #     break  # 超过时间限制，停止读取
         
         if topic == '/kuavo_arm_traj':
-            # cmd_joint.append(msg.position)
             cmd_joint_time_stamp.append(msg.header.stamp)
-            cmd_joint.append(np.radians(msg.position)[:7])
+            cmd_joint.append(np.radians(msg.position)[:])
 
         elif topic == '/robot_arm_q_v_tau':
-            # 将弧度转换为角度
-            # state_joint.append(np.rad2deg(msg.q))
             state_joint_time_stamp.append(msg.header.stamp)
-            state_joint.append((msg.q)[:7])
+            state_joint.append((msg.q)[:])
             
         elif topic=='/drake_ik/cmd_arm_hand_pose':
             cmd_eef_pose_time_stamp.append(msg.header.stamp)
-            xyz=np.array(msg.left_pose.pos_xyz)
-            xyzw=np.array(msg.left_pose.quat_xyzw)
-            rotation = R.from_quat(xyzw)
-            cmd_rot_matrix.append(rotation.as_matrix())  
-            # 转换为欧拉角 (默认是 'xyz' 顺序，单位是弧度)
-            euler_angles = rotation.as_euler('xyz')
-            xyzrpy=np.concatenate((xyz,euler_angles))
-            cmd_eef_pose.append(xyzrpy)
-
+            left_right_eef = [
+                np.concatenate((np.array(pose.pos_xyz), R.from_quat(pose.quat_xyzw).as_euler('xyz')))
+                for pose in [msg.left_pose, msg.right_pose]
+            ]
+            cmd_eef_pose.append(np.concatenate(left_right_eef))
+            
+            
         elif topic=='/drake_ik/real_arm_hand_pose':
             state_eef_pose_time_stamp.append(msg.header.stamp)
-            xyz=np.array(msg.left_pose.pos_xyz)
-            xyzw=np.array(msg.left_pose.quat_xyzw)
-            rotation = R.from_quat(xyzw)
-            state_rot_matrix.append(rotation.as_matrix())  
-            # 转换为欧拉角 (默认是 'xyz' 顺序，单位是弧度)
-            euler_angles = rotation.as_euler('xyz')
-            xyzrpy=np.concatenate((xyz,euler_angles))
-            state_eef_pose.append(xyzrpy)
+            left_right_eef = [
+                np.concatenate((np.array(eef.pos_xyz), R.from_quat(eef.quat_xyzw).as_euler('xyz')))
+                for eef in [msg.left_pose, msg.right_pose]
+            ]
+            state_eef_pose.append(np.concatenate(left_right_eef))
 
         elif topic=='/robot_hand_eff':
             cmd_hand_time_stamp.append(msg.header.stamp)
-            left_hand_pose=msg.data
-            if left_hand_pose[-1]==0:
-                grip=0
-            elif left_hand_pose[-1]==90:
-                grip=1
-            else:
-                print("hand pose error")
-            cmd_hand.append(grip)
+            dex_hand=msg.data
+            l_bi_grip = 0 if dex_hand[5] == 0 else 1
+            r_bi_grip = 0 if dex_hand[11] == 0 else 1
+            bi_grip = [l_bi_grip] + [r_bi_grip]
+            cmd_hand.append(bi_grip)
 
         elif topic=='/robot_hand_position':
             state_hand_time_stamp.append(msg.header.stamp)
+            dex_hand= [float(i) for i in msg.left_hand_position + msg.right_hand_position]
+            l_bi_grip = 0 if dex_hand[5] == 0 else 1
+            r_bi_grip = 0 if dex_hand[11] == 0 else 1
+            bi_grip = [l_bi_grip] + [r_bi_grip]
+            state_hand.append(bi_grip)
 
-            left_hand_pose=msg.left_hand_position
-            if left_hand_pose[-1]==0:
-                grip=0
-            elif left_hand_pose[-1]==90:
-                grip=1
-            else:
-                print("hand pose error")
-            state_hand.append(grip)
-
-        elif topic=='/head_camera/color/image_raw/compressed':
+        elif topic=='/camera1/color/image_raw/compressed':
             img_stamp.append(msg.header.stamp)
             np_arr = np.frombuffer(msg.data, np.uint8)
             cv_img = cv2.imdecode(np_arr, cv2.IMREAD_COLOR)  # 使用 cv2 解码图像
             cv_img = cv2.cvtColor(cv_img, cv2.COLOR_BGR2RGB)
             # cv_img = bridge.imgmsg_to_cv2(msg, desired_encoding='rgb8')
-            resized_img = cv2.resize(cv_img, (96, 96))
-            # 将调整后的图像添加到 img 列表
+            resized_img = cv2.resize(cv_img, (256, 256))
             img.append(resized_img)
-            # cv2.imshow("s",cv_img)
-            # cv2.waitKey(1)
 
+        elif topic=='/camera2/color/image_raw/compressed':
+            img02_stamp.append(msg.header.stamp)
+            np_arr = np.frombuffer(msg.data, np.uint8)
+            cv_img = cv2.imdecode(np_arr, cv2.IMREAD_COLOR)  # 使用 cv2 解码图像
+            cv_img = cv2.cvtColor(cv_img, cv2.COLOR_BGR2RGB)
+            # cv_img = bridge.imgmsg_to_cv2(msg, desired_encoding='rgb8')
+            resized_img = cv2.resize(cv_img, (256, 256))
+            img02.append(resized_img)
+           
     bag.close()
     cmd_eef_pose=adjust_pose_rpy(cmd_eef_pose)
     state_eef_pose=adjust_pose_rpy(state_eef_pose)
 
-    plot_euler_error(cmd_rot_matrix,state_rot_matrix,base_name)
-    plot_euler_error_direct(cmd_eef_pose,state_eef_pose,base_name)
+    # plot_euler_error(cmd_rot_matrix,state_rot_matrix,base_name)
+    # plot_euler_error_direct(cmd_eef_pose,state_eef_pose,base_name)
     
     # 安全判断
     if len(cmd_joint) == 0 or len(state_joint) == 0:
@@ -218,7 +232,8 @@ def use_rosbag_to_show(bag_name):
     aligned_cmd_hand=[]
     aligned_cmd_eef_pose=[]
     aligned_state_eef_pose=[]
-    
+    aligned_img02=[]
+
     drop=2
     img=img[drop:-drop]
     img_stamp=img_stamp[drop:-drop]
@@ -242,22 +257,33 @@ def use_rosbag_to_show(bag_name):
 
         idx_h=np.argmin(np.abs(np.array([t.to_sec() for t in state_eef_pose_time_stamp]) - stamp_sec))
         aligned_state_eef_pose.append(state_eef_pose[idx_h])
-    
+
+        idx_img2=np.argmin(np.abs(np.array([t.to_sec() for t in img02_stamp]) - stamp_sec))
+        aligned_img02.append(img02[idx_img2])
 
     aligned_cmd_joint = [list(item) for item in aligned_cmd_joint]
     aligned_state_joint = [list(item) for item in aligned_state_joint]
     aligned_cmd_eef_pose=[list(item) for item in aligned_cmd_eef_pose]
     aligned_state_eef_pose=[list(item) for item in aligned_state_eef_pose]
-
+    
     print("all length==============>:\nimg_stamp,aligned_cmd_joint,aligned_state_joint,aligned_cmd_eef_pose,aligned_state_eef_pose,aligned_cmd_hand,aligned_state_hand")
     print(len(img_stamp),len(aligned_cmd_joint),len(aligned_state_joint),len(aligned_cmd_eef_pose),len(aligned_state_eef_pose),len(aligned_cmd_hand),len(aligned_state_hand))
     assert len(img_stamp)==len(aligned_cmd_joint)==len(aligned_state_joint)==len(aligned_cmd_eef_pose)==len(aligned_state_eef_pose)==len(aligned_cmd_hand)==len(aligned_state_hand)
     
     for i in range(len(img_stamp)):
-        aligned_cmd_joint[i].append(aligned_cmd_hand[i])
-        aligned_state_joint[i].append(aligned_state_hand[i])
-        aligned_cmd_eef_pose[i].append(aligned_cmd_hand[i])
-        aligned_state_eef_pose[i].append(aligned_state_hand[i])
+        # insert hand[0] ,hand[1] to joint[6], joint[13]
+        aligned_cmd_joint[i].insert(6,aligned_cmd_hand[i][0])
+        aligned_cmd_joint[i].insert(13,aligned_cmd_hand[i][1])
+        aligned_state_joint[i].insert(6,aligned_state_hand[i][0])
+        aligned_state_joint[i].insert(13,aligned_state_hand[i][1])
+        aligned_cmd_eef_pose[i].insert(6,aligned_cmd_hand[i][0])
+        aligned_cmd_eef_pose[i].insert(13,aligned_cmd_hand[i][1])
+        aligned_state_eef_pose[i].insert(6,aligned_state_hand[i][0])
+        aligned_state_eef_pose[i].insert(13,aligned_state_hand[i][1])
+        # aligned_cmd_joint[i].append(aligned_cmd_hand[i])
+        # aligned_state_joint[i].append(aligned_state_hand[i])
+        # aligned_cmd_eef_pose[i].append(aligned_cmd_hand[i])
+        # aligned_state_eef_pose[i].append(aligned_state_hand[i])
 
  # s 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15
  # a 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15
@@ -269,10 +295,12 @@ def use_rosbag_to_show(bag_name):
     aligned_state_eef_pose=np.array(aligned_state_eef_pose)[::jump].astype(np.float32)
     aligned_delta_cmd_eef_pose=None
     img=img[::jump]
+    aligned_img02=aligned_img02[::jump]
+
 
     print("after jump, all length==============>:")
-    print(len(img),len(aligned_state_eef_pose),len(aligned_state_joint),len(aligned_cmd_joint))
-    assert len(img)==len(aligned_state_eef_pose)==len(aligned_cmd_eef_pose)==len(aligned_state_joint)==len(aligned_cmd_joint)
+    print(len(img),len(aligned_img02),len(aligned_state_eef_pose),len(aligned_state_joint),len(aligned_cmd_joint))
+    assert len(img)==len(aligned_img02)==len(aligned_state_eef_pose)==len(aligned_cmd_eef_pose)==len(aligned_state_joint)==len(aligned_cmd_joint)
 
     aligned_cmd_joint=aligned_cmd_joint[1:]
     aligned_state_joint=aligned_state_joint[1:]
@@ -280,9 +308,11 @@ def use_rosbag_to_show(bag_name):
     aligned_state_eef_pose=aligned_state_eef_pose[1:]
     aligned_delta_cmd_eef_pose=aligned_cmd_eef_pose[1:]-aligned_cmd_eef_pose[:-1]
     aligned_delta_cmd_eef_pose[:,6]=aligned_cmd_eef_pose[1:,6]
+    aligned_delta_cmd_eef_pose[:,13]=aligned_cmd_eef_pose[1:,13]
     aligned_cmd_eef_pose=aligned_cmd_eef_pose[1:]
     img=img[1:]
-    
+    aligned_img02=aligned_img02[1:]
+
     print("after delete firet frame==============>:")
     print(len(img),len(aligned_state_eef_pose),len(aligned_state_joint),len(aligned_cmd_joint),len(aligned_delta_cmd_eef_pose),len(aligned_cmd_eef_pose))
 
@@ -290,8 +320,8 @@ def use_rosbag_to_show(bag_name):
     # import matplotlib
     # matplotlib.use('Agg')
     # 创建3行5列的图表并进行比较
-    num_plots = min(len(aligned_cmd_eef_pose[0]), len(aligned_state_eef_pose[0]), 15)  # 限制最多只显示15个数据对比
-    fig, axs = plt.subplots(3, 5, figsize=(32, 18))
+    num_plots = min(len(aligned_state_joint[0]), len(aligned_state_eef_pose[0]), 16)  # 限制最多只显示15个数据对比
+    fig, axs = plt.subplots(3, 6, figsize=(64, 36))
     fig.suptitle(base_name, fontsize=16)
     for i in range(num_plots):
         kuavo_position = [data[i] for data in aligned_cmd_joint]
@@ -300,8 +330,8 @@ def use_rosbag_to_show(bag_name):
         cmd_eef=[data[i] for data in aligned_cmd_eef_pose]
         state_eef=[data[i] for data in aligned_state_eef_pose]
         cmd_eef_delta=[data[i] for data in aligned_delta_cmd_eef_pose]
-        row = i // 5
-        col = i % 5
+        row = i // 6
+        col = i % 6
         # axs[row, col].plot(kuavo_position, label='/kuavo_arm_traj')
         # axs[row, col].plot(robot_q, label='/robot_arm_q_v_tau')
         axs[row, col].plot(cmd_eef, label='/cmd_eef')
@@ -316,7 +346,8 @@ def use_rosbag_to_show(bag_name):
           "\n state_joint:",aligned_state_joint[exampl_index],
           "\n aligned_delta_cmd_eef_pose:",aligned_delta_cmd_eef_pose[exampl_index],
           "\n state_eef:",aligned_state_eef_pose[exampl_index],
-          "\n img shape:",img[exampl_index].shape)   
+          "\n img shape:",img[exampl_index].shape,
+          "\n aligned_img02 shape:",aligned_img02[exampl_index].shape)   
 
     plt.tight_layout()
     
@@ -336,11 +367,11 @@ def use_rosbag_to_show(bag_name):
     
     # output_zarr_path = f"{save_zarr_folder}/{base_name}.zarr"
     
-    return img,aligned_state_eef_pose,aligned_delta_cmd_eef_pose,aligned_cmd_eef_pose,aligned_state_joint,aligned_cmd_joint
+    return img,aligned_img02,aligned_state_eef_pose,aligned_delta_cmd_eef_pose,aligned_cmd_eef_pose,aligned_state_joint,aligned_cmd_joint
 
 if __name__ == "__main__":
-    bag_folder_name = "BJ_juice1"
-    bag_folder_path = "/home/lab/hanxiao/" + bag_folder_name
+    bag_folder_name = "test"
+    bag_folder_path = "/home/lab/hanxiao/dataset/kuavo/task_test/" + bag_folder_name
     
     save_plt_folder = f"{bag_folder_path}/plt"
     save_lastPic_folder = f"{bag_folder_path}/last_pic"
@@ -360,22 +391,26 @@ if __name__ == "__main__":
         start_time = time.time()
         print("current path", path)
         seed = replay_buffer.n_episodes
-        img01, eef_s, delta_eef_a, eef_a, joint_s, joint_a = use_rosbag_to_show(path)
-        data = list(zip(img01, eef_s, delta_eef_a))
-        grouped_data = data
+        img01,aligned_img02, eef_s, delta_eef_a, eef_a, joint_s, joint_a = use_rosbag_to_show(path)
+        data = list(zip(img01,aligned_img02, eef_s, delta_eef_a, eef_a, joint_s, joint_a))
+        
         episode = []
-        for i, (img01, state, action) in enumerate(grouped_data):
+        for i, (img01, img02, eef_s, delta_eef_a, eef_a, joint_s, joint_a,) in enumerate(data):
             episode.append({
-                'img': img01,
-                'state': state,
-                'action': action,
+                'img01': img01,
+                'img02': img02,
+                'eef_s': eef_s,
+                'delta_eef_a': delta_eef_a,
+                'eef_a': eef_a,
+                'joint_s': joint_s,
+                'joint_a': joint_a,
             })
 
         print("episode length", len(episode))
         print("episode keys", episode[0].keys())
-        print("episode[0]['img'].shape", episode[0]['img'].shape)
-        print("episode[0]['state'].shape", episode[0]['state'].shape)
-        print("episode[0]['action'].shape", episode[0]['action'].shape)
+        print("episode[0]['img'].shape", episode[0]['img01'].shape)
+        print("episode[0]['state'].shape", episode[0]['eef_s'].shape)
+        print("episode[0]['action'].shape", episode[0]['delta_eef_a'].shape)
 
         data_dict = dict()
         for key in episode[0].keys():
